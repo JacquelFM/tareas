5e34cdfc3d39a03d5a9395801972bc71f9312cda
JacquelineLK119 <jflores43@ucol.mx>
2018-09-30 23:31:26 -0500

    Estilo actualizado

--- tarea2.js
+++ tarea2.js
@@ -3,6 +3,7 @@ var btnRestar = document.querySelector('#restar');
 var btnMultiplicar = document.querySelector('#multiplicar');
 var btnDividir = document.querySelector('#dividir');
 var btnResiduo = document.querySelector('#residuo');
+var lblSigno = document.querySelector('#signo');
 
 var inputNumero1 = document.querySelector('#numero1');
 var inputNumero2 = document.querySelector('#numero2');
@@ -16,26 +17,31 @@ btnDividir.addEventListener('click', dividir);
 btnResiduo.addEventListener('click', residuo);
 
 function sumar() {
+    lblSigno.textContent = btnSumar.textContent;
     resultado = Number(inputNumero1.value) + Number(inputNumero2.value);
     inputResultado.value = resultado;
 }
 
 function restar() {
+    lblSigno.textContent = btnRestar.textContent;
     resultado = Number(inputNumero1.value) - Number(inputNumero2.value);
     inputResultado.value = resultado;
 }
 
 function multiplicar() {
+    lblSigno.textContent = btnMultiplicar.textContent;
     resultado = Number(inputNumero1.value) * Number(inputNumero2.value);
     inputResultado.value = resultado;
 }
 
 function dividir() {
+    lblSigno.textContent = btnDividir.textContent;
     resultado = Number(inputNumero1.value) / Number(inputNumero2.value);
     inputResultado.value = resultado;
 }
 
 function residuo() {
+    lblSigno.textContent = btnResiduo.textContent;
     resultado = Number(inputNumero1.value) % Number(inputNumero2.value);
     inputResultado.value = resultado;
 }
